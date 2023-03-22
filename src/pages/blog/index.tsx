2772f953bc09a95fef7a5781f3e65d79f39e1059
dgrodriguez24 <dgrodriguez24@hotmail.com>
2023-03-22 12:22:48 -0400

    navigate: Link component navigation

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function Blog() {
+    return (
+        <div>
+            <Link href="/">Back to Home</Link>
+            <h1>Blog Homepage</h1>
+        </div>
+    );
+}
+
+export default Blog;
