2e3d8621a0e408f023c077280458687b0a26328b
dgrodriguez24 <dgrodriguez24@hotmail.com>
2023-04-21 14:31:16 -0400

    more on getStaticProps

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,73 +1,57 @@
 /*
-In the previous lesson we learned about Static Generation 
-w/ data fetching. We learned about the getStaticProps() function
-which we can use to inject props to our component. 
+More on getStaticProps()
 
-Now although this works well, it is typically now how you would
-write production application. For example, the JSX for a User component 
-would be written in its own file and invoked inside the UserList 
-component, passing in the necessary props.
+Previously we learned about static generation with data fetching 
+using the getStaticProps() function. We were able to fetch data from 
+an API and provide it as props to the page by returning an object from 
+the function. 
 
-In this lesson, let's discuss about that. 
+In this lesson we want to highlight a few important details 
+about the getStaticProps() function. 
 
-Now what we need is a seperate component called User, which will accept
-a user's details as props and render the username and email. 
+1. 
+getStaticProps() runs only on the server side. And this is the reason 
+that when we console.log() the data, we see the log in the terminal as 
+opposed to seeing it in the browser console. Which also implies that 
+the function will never run client-side. 
 
-For that, we need to create a file. 
+s a matter of fact, the 
+code your write inside getStaticProps() won't event be included 
+in the JS Bundle that is sent to the browser. This leads us to the 
+second point.
 
-But the question is, where do we place that file? Should we 
-create it inside the pages folder? 
+2. 
+You can write server-side code directly in getStaticProps(). So 
+code that you would normally see in Node.js like accessing the 
+file system using the fs() module, or querying the database can all
+be done inside the getStaticProps() function. 
 
-No. 
+And if you were to import lets say, the fs() module to read the file system, the 
+code for fs() module would also not be bundled as part of the code 
+sent to the browser. Next.js is pretty smart when it comes to that. 
 
-When building a Next.js application, pages are very special. They
-give you added benefits like automatic routing, and access to 
-special functions like getStaticProps. However, these 
-benefits shouldn't be available for presentation components
-that we write in our application. Hence, they shouldn't be
-created inside the pages folder. 
+You also don't have to worry about including API keys in getStaticProps() 
+as that wont make it to the browser either.
 
-Instead, we can create them in a separate 'components' folder.
-So within the project root, create a new folder called 'components'. 
-Or, if you are using 'src/' folder, you can create the 
-'components' folder inside the 'src/' folder.
+3.
+The getStaticProps() function is allowed only in a page and 
+cannot be run from a regular component file. It is used only 
+for pre-rendering and not client-side data fetching. From our 
+example, in the previous lesson, we can define getStaticProps() 
+in any file inside the 'pages/' folder, but not in the 
+'components/' folder.
 
-Now, the the folder name is not a strict convention, but it is 
-something that makes sense. 
+4.
+getStaticProps() should return an object and the object should 
+contain a 'props' key which is also an object. Otherwise, Next.js will
+throw an error. In our example, we returned an object, and the object 
+contained a 'props' key which was an object as well. 
 
-Within the './src/components/' folder, create a new file called 'user.tsx'.
-
-In React, the naming convention for file names is Pascal Case, but 
-from what was observed for Next.js, the file names are named in Kebab 
-Case, so we are following the same.
-
-Within the 'user.tsx' file, we define the <User /> component. 
-
-    function User({ user }) {
-        return (
-            <>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-            </>
-        );
-    }
-
-    export default User;
-
-Now back in '.src/pages/users.tsx' we can replace part of the 
-JSX with the <User /> component. 
-
-If you now save the file and look at the browser, the UI remains
-unaffected, but this time our code is organized better.
-
-So what you should take away from this lesson is the difference 
-between a page and a component. If you want to expose some 
-UI as a route in your application create it as a page. If you are 
-simply looking to adhere to the component based architecture 
-and organize your JSX, go with components. 
-
-Components don't need the special features that Next.js provides 
-for pages in an application.
+5.
+getStaticProps() will run at build time, however during development, 
+getStaticProps() runs on every request. You are changing the code almost 
+every minute and it's only logical that Next.js has to run the function 
+on every request. So there is a slight difference between dev and prod.
 */
 
 import User from '../components/user';
